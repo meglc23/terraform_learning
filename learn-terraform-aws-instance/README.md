a0f01285f34d4ed4932ea841214dccd59ee78433
Chen Li <yeeaayee23@gmail.com>
2021-09-07 17:15:18 -0700

    Build and change aws ec2 infra

---
+++ learn-terraform-aws-instance/README.md
@@ -0,0 +1,5 @@
+## Terraform with AWS EC2 Instace
+### Build Infrastructure
+resource: https://learn.hashicorp.com/tutorials/terraform/aws-build?in=terraform/aws-get-started
+### Change Infrastructure
+resource: https://learn.hashicorp.com/tutorials/terraform/aws-change?in=terraform/aws-get-started
---
+++ learn-terraform-aws-instance/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws" # defines an optional hostname, a namespace, and the provider type
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" { # refers Terraform to the AWS credentials stored in your AWS configuration file
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" { # define components of your infrastructure
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
